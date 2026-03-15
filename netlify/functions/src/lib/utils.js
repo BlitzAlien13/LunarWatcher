a748c2c87e510d6c23c4f8bdbd98dc92776d2218
BlitzAlien13 <blitza922@gmail.com>
2026-03-16 00:25:23 +0100

    oko

--- netlify/functions/src/lib/utils.js
+++ netlify/functions/src/lib/utils.js
@@ -1,10 +1,18 @@
-import { PermissionsBitField } from "discord.js";
+import { Permissions } from "discord.js";
 
 /**
  * @param {string | number | bigint} permissions
- * @param {import("discord.js").PermissionsString} permission
+ * @param {import("discord.js").PermissionResolvable} permission
  */
 export function hasPermissions(permissions, permission) {
-  const perms = new PermissionsBitField(permissions);
-  return perms.has(permission);
+  // Discord API sends permissions as a stringified int; use BigInt to avoid precision loss.
+  const perms = new Permissions(BigInt(permissions));
+
+  // discord.js v13 expects permission names in UPPER_SNAKE_CASE.
+  const key =
+    typeof permission === "string"
+      ? permission.replace(/([a-z])([A-Z])/g, "$1_$2").toUpperCase()
+      : permission;
+
+  return perms.has(key);
 }
