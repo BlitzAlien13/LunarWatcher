3e473219da757890588df7a391c63db432c7bd44
BlitzAlien13 <blitza922@gmail.com>
2026-03-15 23:24:30 +0100

    Fix Netlify routing and Mongo init

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,6 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
+import mongoose from "mongoose";
 
 import authRouter from "./src/routes/auth/index.js";
 import dashboardRouter from "./src/routes/dashboard/index.js";
@@ -7,11 +8,37 @@ import levelsRouter from "./src/routes/levels/index.js";
 
 const api = express();
 const router = Router();
+const mongoUri = process.env.MONGODB_URI;
+const mongoDbName = process.env.MONGODB_DB;
+let mongoReadyPromise;
+
+async function ensureMongoConnected() {
+  if (!mongoReadyPromise) {
+    mongoReadyPromise = mongoose.connect(mongoUri, {
+      dbName: mongoDbName || undefined,
+    });
+  }
+  return mongoReadyPromise;
+}
 
 // quick path logger to diagnose routing in Netlify/Express
 api.use((req, _res, next) => {
   console.log("[api]", { originalUrl: req.originalUrl, url: req.url, baseUrl: req.baseUrl, path: req.path });
   next();
+});
+
+// Make sure we have a Mongo connection before handling routes
+api.use(async (req, res, next) => {
+  try {
+    if (!mongoUri) {
+      throw new Error("MONGODB_URI env var is missing");
+    }
+    await ensureMongoConnected();
+    next();
+  } catch (err) {
+    console.error("Mongo connection error", err);
+    res.status(500).send("Database connection error");
+  }
 });
 
 router.get("/hello", (req, res) => res.send("Hello World!"));
